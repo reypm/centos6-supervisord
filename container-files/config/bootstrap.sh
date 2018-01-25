acedd90cb8ebe44e987622b5c04ee22ef3c82e80
Reynier Perez <reynierpm@gmail.com>
2018-01-24 19:59:27 -0500

    Small fixes

--- container-files/config/bootstrap.sh
+++ container-files/config/bootstrap.sh
@@ -6,7 +6,6 @@ set -u
 # Supervisord default params
 SUPERVISOR_PARAMS='-c /etc/supervisord.conf'
 
-
 # Create directories for supervisor's UNIX socket and logs (which might be missing
 # as container might start with /data mounted from another data-container).
 mkdir -p /data/conf /data/run /data/logs
@@ -23,10 +22,10 @@ fi
 if test -t 0; then
   # Run supervisord detached...
   supervisord $SUPERVISOR_PARAMS
-  
+
   # Some command(s) has been passed to container? Execute them and exit.
   # No commands provided? Run bash.
-  if [[ $@ ]]; then 
+  if [[ $@ ]]; then
     eval $@
   else 
     export PS1='[\u@\h : \w]\$ '
@@ -36,12 +35,12 @@ if test -t 0; then
 # Detached mode? Run supervisord in foreground, which will stay until container is stopped.
 else
   # If some extra params were passed, execute them before.
-  # @TODO It is a bit confusing that the passed command runs *before* supervisord, 
+  # @TODO It is a bit confusing that the passed command runs *before* supervisord,
   #       while in interactive mode they run *after* supervisor.
   #       Not sure about that, but maybe when any command is passed to container,
   #       it should be executed *always* after supervisord? And when the command ends,
   #       container exits as well.
-  if [[ $@ ]]; then 
+  if [[ $@ ]]; then
     eval $@
   fi
   supervisord -n $SUPERVISOR_PARAMS
